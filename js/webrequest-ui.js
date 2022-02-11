2a54573cc9a8cb87ce2ebd5f9aa9d8cd170076df
Francesco Bianco <info.francescobianco@gmail.com>
2022-02-11 12:46:34 +0100

    First commit

---
+++ js/webrequest-ui.js
@@ -0,0 +1,9 @@
+
+
+
+//<script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.4.0/build/highlight.min.js"></script>
+//<script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.4.0/build/languages/php.min.js"></script>
+//<script src="//cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.8.0/highlightjs-line-numbers.min.js"></script>
+//<script src="//cdn.jsdelivr.net/npm/shepherd.js@8.3.1/dist/js/shepherd.min.js"></script>
+
+import "../node_modules/"
